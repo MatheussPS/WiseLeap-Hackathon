27eaa67801005faf328f11ab34e543b3e859b75f
Matheus Pinter <matheuspinter08@gmail.com>
2024-05-11 13:23:16 -0300

    feat:Finished the header

--- js/script.js
+++ js/script.js
@@ -1,19 +1,22 @@
 const openSideMenu = () => {
     const barraLateral = document.querySelector('.side-menu')
     const actualDisplay = barraLateral.style.display
+    const hamburguer = document.querySelector(".hamburguer")
+    const body = document.querySelector("html")
+
+
     if(actualDisplay == 'block'){
         barraLateral.style.display = "none"
+        hamburguer.src = "img/hamburguer-img.png"
+        body.style.backgroundColor = "#fff"
     }
     else{
         barraLateral.style.display = "block"
+        hamburguer.src = "img/close-side-bar.png"
+        body.style.backgroundColor = "rgba(0, 0, 0, 0.5)"
     }
-    
+
 
 }
 
-window.onclick = function (event) {
-    const barraLateral = document.querySelector('.side-menu')
-    if (event.target == barraLateral){
-        openSideMenu()
-    }
-}
+
