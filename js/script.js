cecebed98e06937ac4fd18048517612c9aa45269
Paulo Sergio <pscosta2008@gmail.com>
2024-05-12 13:48:28 -0300

    fix: fixing header style

--- js/script.js
+++ js/script.js
@@ -3,17 +3,19 @@ const openSideMenu = () => {
     const actualDisplay = barraLateral.style.display
     const hamburguer = document.querySelector(".hamburguer")
     const body = document.querySelector("body")
+    const bkgSideMenu = document.querySelector('.bkg-side-menu')
 
 
     if(actualDisplay == 'block'){
         barraLateral.style.display = "none"
-        hamburguer.src = "img/hamburguer-img.png"
+        bkgSideMenu.style.display = "none"
+        hamburguer.src = "img/hamburguer-img2.png"
         body.style.backgroundColor = "#fff"
     }
     else{
         barraLateral.style.display = "block"
+        bkgSideMenu.style.display = "block"
         hamburguer.src = "img/close-side-bar.png"
-        body.style.backgroundColor = "rgba(0, 0, 0, 0.5)"
     }
 
 
